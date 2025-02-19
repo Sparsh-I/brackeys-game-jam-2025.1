672f7c30b24c95b6a9e44157cdb23c8e10aec04b
Sparsh-I <sparsh.poonacha@gmail.com>
2025-02-19 02:17:05 -0800

    Added basic functionality
    Added 1x1 items, fixed picking up and dropping items, added spawning items, fixed grid

--- Brackeys Game Jam 2025.1/Assets/Scripts/GridController.cs
+++ Brackeys Game Jam 2025.1/Assets/Scripts/GridController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class GridController : MonoBehaviour
 {
-    public int gridWidth = 5, gridHeight = 5;
-    public float cellSize = 1f;
+    [SerializeField] private int gridWidth, gridHeight;
+    [SerializeField] private float cellSize;
+    [SerializeField] private float offset;
     public Transform cursor;
     private Vector2Int _cursorPos = Vector2Int.zero;
+    
+    [SerializeField] private GameObject[] itemPrefabs;
 
-    private Item _heldItem = null;
+    private Item _heldItem;
     private Item[,] _grid;
     
     // Start is called before the first frame update
@@ -18,6 +22,7 @@ public class GridController : MonoBehaviour
     {
         _grid = new Item[gridWidth, gridHeight];
         UpdateCursorPosition();
+        SpawnItems();
     }
 
     // Update is called once per frame
@@ -25,8 +30,17 @@ public class GridController : MonoBehaviour
     {
         HandleMovement();
         if (Input.GetKeyDown(KeyCode.Return)) HandleItemPickupPlacement();
+        
+        if (IsRowEmpty(6)) SpawnItems();
     }
-
+    
+    private bool IsRowEmpty(int row)
+    {
+        for (int x = 0; x < gridWidth; x++)
+            if (_grid[x, row]) return false;
+        return true;
+    }
+    
     private void HandleMovement()
     {
         Vector2Int move = Vector2Int.zero;
@@ -36,13 +50,66 @@ public class GridController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D)) move.x = 1;
         
         Vector2Int newPos = _cursorPos + move;
-        UpdateCursorPosition();
+        if (IsInsideGrid(newPos)) 
+        {
+            _cursorPos = newPos;
+            UpdateCursorPosition();
+        }
     }
 
     private void UpdateCursorPosition()
     {
-        cursor.position = new Vector3(_cursorPos.x * cellSize, _cursorPos.y * cellSize, 0);
+        Vector3 gridOffset = new Vector3(-3 * cellSize, -3 * cellSize, 0);
+        cursor.position = new Vector3(_cursorPos.x * cellSize, _cursorPos.y * cellSize, 0) + gridOffset;
+    }
+
+    private void HandleItemPickupPlacement()
+    {
+        if (_heldItem)
+        {
+            if (!_grid[_cursorPos.x, _cursorPos.y])
+            {
+                _grid[_cursorPos.x, _cursorPos.y] = _heldItem;
+                _heldItem.transform.position = cursor.position;
+                _heldItem.gameObject.SetActive(true);
+                _heldItem = null;
+            }
+        }
+        else
+        {
+            if (_grid[_cursorPos.x, _cursorPos.y])
+            {
+                _heldItem = _grid[_cursorPos.x, _cursorPos.y];
+                _grid[_cursorPos.x, _cursorPos.y] = null;
+                _heldItem.gameObject.SetActive(false);
+            }
+        }
     }
     
-    private void HandleItemPickupPlacement()
+    private bool IsInsideGrid(Vector2Int pos)
+    {
+        return pos.x >= 0 && pos.x < gridWidth && 
+               pos.y >= 0 && pos.y < gridHeight;
+    }
+
+    private void SpawnItems()
+    {
+        for (int i = 1; i < 6; i += 2)
+        {
+            if (!_grid[i, 6])
+            {
+                int randomIdx = Random.Range(0, itemPrefabs.Length);
+                GameObject itemPrefab = itemPrefabs[randomIdx];
+
+                Vector3 worldPos = GridToWorldPosition(i, 6);
+                GameObject newItem = Instantiate(itemPrefab, worldPos, Quaternion.identity);
+                _grid[i, 6] = newItem.GetComponent<Item>();
+            }
+        }
+    }
+    
+    private Vector3 GridToWorldPosition(int x, int y)
+    {
+        return transform.position + new Vector3(x * cellSize, y * cellSize, 0);
+    }
 }
---
+++ Brackeys Game Jam 2025.1/Assets/Scripts/Item.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Vector2Int size = Vector2Int.one;
+}
