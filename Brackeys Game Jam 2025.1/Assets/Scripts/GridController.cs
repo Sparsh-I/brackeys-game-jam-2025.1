6d5c93a5e78df319df60f5bb2b9d13413069c0e9
Sparsh-I <sparsh.poonacha@gmail.com>
2025-02-19 23:14:20 -0800

    Added highlight to indicate where item will be placed

--- Brackeys Game Jam 2025.1/Assets/Scripts/GridController.cs
+++ Brackeys Game Jam 2025.1/Assets/Scripts/GridController.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.VFX;
 
 public class GridController : MonoBehaviour
 {
     [SerializeField] private int gridWidth, gridHeight;
     [SerializeField] private float cellSize;
     [SerializeField] private float offset;
+    private Vector3 gridOffset;
+    
     public Transform cursor;
     private Vector2Int _cursorPos = Vector2Int.zero;
+    [SerializeField] private Transform highlight;
     
     [SerializeField] private GameObject[] itemPrefabs;
+    [SerializeField] private int spawnRow;
 
     private Item _heldItem;
     private Item[,] _grid;
@@ -28,10 +28,16 @@ public class GridController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool validPos = (_cursorPos.y < 4 | _cursorPos.y == 5) & 
+                        !((_cursorPos.x % 2 == 1 | _cursorPos.x == 6) 
+                          & _cursorPos.y == 5);
         HandleMovement();
-        if (Input.GetKeyDown(KeyCode.Return)) HandleItemPickupPlacement();
+        if (Input.GetKeyDown(KeyCode.Return) & validPos) HandleItemPickupPlacement();
         
-        if (IsRowEmpty(6)) SpawnItems();
+        if (IsRowEmpty(spawnRow)) SpawnItems();
+
+        if (validPos) highlight.gameObject.SetActive(_heldItem);
+        // UpdateHighlight();
     }
     
     private bool IsRowEmpty(int row)
@@ -59,32 +65,92 @@ public class GridController : MonoBehaviour
 
     private void UpdateCursorPosition()
     {
-        Vector3 gridOffset = new Vector3(-3 * cellSize, -3 * cellSize, 0);
+        gridOffset = new Vector3(-3 * cellSize, -3 * cellSize, 0);
         cursor.position = new Vector3(_cursorPos.x * cellSize, _cursorPos.y * cellSize, 0) + gridOffset;
+
+        if (highlight) highlight.position = cursor.position;
     }
+
+    /* private bool CanPlaceItem(Item item, Vector2Int cursorPos)
+    {
+        foreach (var cell in item.occupiedCells)
+        {
+            Vector2Int targetCell = cell + cursorPos;
+            if (targetCell.x < 0 
+                || targetCell.x >= gridWidth
+                || targetCell.y < 0 
+                || targetCell.y > spawnRow - 2 
+                || _grid[targetCell.x, targetCell.y])
+                return false;
+        }
+
+        return true;
+    } */
 
     private void HandleItemPickupPlacement()
     {
-        if (_heldItem)
+        if (_heldItem)                                  // currently holding an item
         {
-            if (!_grid[_cursorPos.x, _cursorPos.y])
+            if (!_grid[_cursorPos.x, _cursorPos.y])     // hovering over an empty space
             {
-                _grid[_cursorPos.x, _cursorPos.y] = _heldItem;
+                /* if (CanPlaceItem(_heldItem, _cursorPos))
+                {
+                    foreach (var cell in _heldItem.occupiedCells)
+                    {
+                        Vector2Int targetCell = cell + _cursorPos;
+                        _grid[targetCell.x, targetCell.y] = _heldItem;
+                    }
+                } */
                 _heldItem.transform.position = cursor.position;
+                _grid[_cursorPos.x, _cursorPos.y] = _heldItem;
+                if (_cursorPos.y == spawnRow) 
+                    _heldItem.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+                else
+                    _heldItem.transform.localScale = new Vector3(cellSize, cellSize, 1);
                 _heldItem.gameObject.SetActive(true);
                 _heldItem = null;
+                
+                if (_heldItem) highlight.transform.localScale = new Vector3(_heldItem.width, _heldItem.height, 1);
             }
         }
-        else
+        else                                            // NOT holding an item
         {
-            if (_grid[_cursorPos.x, _cursorPos.y])
+            if (_grid[_cursorPos.x, _cursorPos.y])      // hovering over an item
             {
                 _heldItem = _grid[_cursorPos.x, _cursorPos.y];
-                _grid[_cursorPos.x, _cursorPos.y] = null;
+                /* foreach (var cell in _heldItem.occupiedCells)
+                {
+                    Vector2Int targetCell = _cursorPos + cell;
+                    _grid[targetCell.x, targetCell.y] = null;
+                } */
                 _heldItem.gameObject.SetActive(false);
+                _grid[_cursorPos.x, _cursorPos.y] = null;
+                
+                if (_heldItem) highlight.transform.localScale = new Vector3(_heldItem.width, _heldItem.height, 1);
             }
         }
     }
+
+    /* private void UpdateHighlight()
+    {
+        if (_heldItem)
+        {
+            int minWidth = 1, minHeight = 1;
+            foreach (var cell in _heldItem.occupiedCells)
+            {
+                maxWidth = Mathf.Max(minWidth, cell.x + 1);
+                maxHeight = Mathf.Max(minHeight, cell.y + 1);
+            }
+
+            highlight.localScale = new Vector3(minWidth * cellSize, minHeight * cellSize, 1);
+
+            highlight.position = GridToWorldPosition(_cursorPos.x, _cursorPos.y);
+        }
+        else
+        {
+            highlight.localScale = Vector3.zero; // no highlight if no item is held
+        }
+    } */
     
     private bool IsInsideGrid(Vector2Int pos)
     {
@@ -94,22 +160,20 @@ public class GridController : MonoBehaviour
 
     private void SpawnItems()
     {
-        for (int i = 1; i < 6; i += 2)
+        for (int i = 0; i < 6; i += 2)
         {
-            if (!_grid[i, 6])
-            {
-                int randomIdx = Random.Range(0, itemPrefabs.Length);
-                GameObject itemPrefab = itemPrefabs[randomIdx];
+            int randomIdx = Random.Range(0, itemPrefabs.Length);
+            GameObject itemPrefab = itemPrefabs[randomIdx];
 
-                Vector3 worldPos = GridToWorldPosition(i, 6);
-                GameObject newItem = Instantiate(itemPrefab, worldPos, Quaternion.identity);
-                _grid[i, 6] = newItem.GetComponent<Item>();
-            }
+            Vector3 worldPos = GridToWorldPosition(i, spawnRow);
+            GameObject newItem = Instantiate(itemPrefab, worldPos, Quaternion.identity);
+            newItem.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+            _grid[i, spawnRow] = newItem.GetComponent<Item>();
         }
     }
     
     private Vector3 GridToWorldPosition(int x, int y)
     {
-        return transform.position + new Vector3(x * cellSize, y * cellSize, 0);
+        return gridOffset + new Vector3(x * cellSize, y * cellSize, 0);
     }
 }
--- Brackeys Game Jam 2025.1/Assets/Scripts/Item.cs
+++ Brackeys Game Jam 2025.1/Assets/Scripts/Item.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public Vector2Int size = Vector2Int.one;
+    public int width;
+    public int height;
 }
